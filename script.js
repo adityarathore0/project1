427eeb9275a375661ef0a1cc9b76076f9c6b01ca
Aditya Rathor <aditya.2006.rathore.99@gmail.com>
2025-05-16 19:13:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+*{
+    margin: 0%;
+    padding: 0;
+}
+.box {
+    color: white;
+    /* display: flex; 
+    justify-content: center; */
+    /* align-items: center; */
+}
+.container{
+    width: 100%;
+    height: 90vh;
+    background-color: red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.box1{
+    font-size:5rem;
+}
+.box2 {
+    font-size: 2rem;
+}
+.box3 {
+    margin: 0.5rem;
+    margin-left: 0.2rem;
+}
+nav{
+    background-color: white;  
+    height: 150px;
+}
+
+ .search {
+    font-size: 1.3rem;
+}
+nav form button {
+    font-size: 1.2rem;
+}
